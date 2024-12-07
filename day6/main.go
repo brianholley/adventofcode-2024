3fa4e6c44da349ddd38941dff13a5bff2b0bad59
Brian Holley <bholley@stripe.com>
2024-12-06 23:06:23 -0800

    Add day 6, part 2 using brute force method for now

--- day6/main.go
+++ day6/main.go
@@ -15,13 +15,18 @@ func main() {
 type guardPos struct {
 	row       int
 	col       int
-	direction int
+	direction string
 }
 
-const north = 1
-const east = 2
-const south = 3
-const west = 4
+type cell struct {
+	row int
+	col int
+}
+
+const north = "N"
+const east = "E"
+const south = "S"
+const west = "W"
 
 func part1(stdin *bufio.Scanner) string {
 	result := 0
@@ -84,7 +89,84 @@ func part1(stdin *bufio.Scanner) string {
 	return fmt.Sprint(result)
 }
 
+// Brute force solution :(
+func encountersLoop(areaMap [][]string, guard guardPos) bool {
+	mapWidth := len(areaMap)
+	mapHeight := len(areaMap[0])
+
+	for {
+		if guard.row < 0 || guard.row >= mapHeight || guard.col < 0 || guard.col >= mapWidth {
+			return false
+		}
+		if strings.Contains(areaMap[guard.row][guard.col], guard.direction) {
+			return true
+		}
+		areaMap[guard.row][guard.col] = areaMap[guard.row][guard.col] + guard.direction
+		if guard.direction == north {
+			if guard.row > 0 && areaMap[guard.row-1][guard.col] == "#" {
+				guard.direction = east
+			} else {
+				guard.row--
+			}
+		} else if guard.direction == east {
+			if guard.col < mapWidth-1 && areaMap[guard.row][guard.col+1] == "#" {
+				guard.direction = south
+			} else {
+				guard.col++
+			}
+		} else if guard.direction == south {
+			if guard.row < mapHeight-1 && areaMap[guard.row+1][guard.col] == "#" {
+				guard.direction = west
+			} else {
+				guard.row++
+			}
+		} else if guard.direction == west {
+			if guard.col > 0 && areaMap[guard.row][guard.col-1] == "#" {
+				guard.direction = north
+			} else {
+				guard.col--
+			}
+		}
+	}
+}
+
 func part2(stdin *bufio.Scanner) string {
 	result := 0
+
+	originalMap := [][]string{}
+	originalGuard := guardPos{}
+	for i := 0; stdin.Scan(); i++ {
+		line := stdin.Text()
+		originalMap = append(originalMap, strings.Split(line, ""))
+
+		if strings.Contains(line, "^") {
+			originalGuard.row = i
+			originalGuard.col = strings.Index(line, "^")
+			originalGuard.direction = north
+		}
+	}
+
+	objects := []cell{}
+
+	for i := range originalMap {
+		for j := range originalMap[i] {
+
+			if originalMap[i][j] == "." {
+				areaMap := lib.Array2dCopy(originalMap)
+				guard := originalGuard
+
+				areaMap[i][j] = "#"
+				if encountersLoop(areaMap, guard) {
+					objects = append(objects, cell{row: i, col: j})
+				}
+			}
+		}
+	}
+
+	// for _, o := range objects {
+	// 	fmt.Println(o)
+	// }
+	result = len(objects)
+
 	return fmt.Sprint(result)
 }
