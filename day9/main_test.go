197552a247da0f0512eec6eeb6783706f4cd1700
Brian Holley <brian.holley@hotmail.com>
2024-12-11 22:06:38 -0800

    Day 9, parts 1 and 2

---
+++ day9/main_test.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"bufio"
+	"os"
+	"testing"
+)
+
+const part1Expected = "1928"
+const part1Answer = "6385338159127"
+const part2Expected = "2858"
+const part2Answer = "6415163624282"
+
+func loadfile(filename string) *bufio.Scanner {
+	file, _ := os.Open(filename)
+	return bufio.NewScanner(file)
+}
+
+func Test_Part1_Sample(t *testing.T) {
+	scanner := loadfile("sample.txt")
+	actual := part1(scanner)
+	if part1Expected != actual {
+		t.Errorf(`Part 1 sample incorrect. Expected: %s, Actual: %s`, part1Expected, actual)
+	}
+}
+
+func Test_Part2_Sample(t *testing.T) {
+	scanner := loadfile("sample.txt")
+	actual := part2(scanner)
+	if part2Expected != actual {
+		t.Errorf(`Part 2 sample incorrect. Expected: %s, Actual: %s`, part2Expected, actual)
+	}
+}
+
+func Test_Part1_Answer(t *testing.T) {
+	scanner := loadfile("input.txt")
+	actual := part1(scanner)
+	if part1Answer != actual {
+		t.Errorf(`Part 1 answer incorrect. Expected: %s, Actual: %s`, part1Expected, actual)
+	}
+}
+
+func Test_Part2_Answer(t *testing.T) {
+	scanner := loadfile("input.txt")
+	actual := part2(scanner)
+	if part2Answer != actual {
+		t.Errorf(`Part 2 answer incorrect. Expected: %s, Actual: %s`, part2Expected, actual)
+	}
+}
